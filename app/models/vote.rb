a15be32bc700811642aa47fda82ac2c64d069665
Corey Feng <corey@xfers.com>
2020-04-17 15:42:47 +0800

    Add voting function

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,2 +1,9 @@
 class Vote < ApplicationRecord
+  belongs_to :charity
+  enum status: {
+    created: "created",
+    xfers_processing: "xfers_processing",
+    completed: "completed",
+    failed: "failed",
+  }
 end
