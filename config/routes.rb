642eea776b6e711bf0559cde67e609db6286b4d4
Juan <juanyong@xfers.com>
2020-04-17 14:13:41 +0800

    Redefine routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :charities, only: [:index, :show, :create, :update, :delete]
+  get 'charities', to: 'charities#index'
+  get '/charities/new', to: 'charities#new'
+  get '/charities/:id', to: 'charities#show'
+
+  post '/charities/new', to: 'charities#create'
+  patch '/charities/:id', to: 'charities#update'
+  post '/charities/delete', to: 'charities#delete'
 end
