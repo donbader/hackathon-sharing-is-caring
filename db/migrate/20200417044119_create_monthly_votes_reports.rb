76a12abafd135022dc360cdc2e38f9a1d90048b8
Lim Swee Tat <sweetat+github@xfers.com>
2020-04-17 15:51:49 +0800

    Changed from charity_id to charity

--- db/migrate/20200417044119_create_monthly_votes_reports.rb
+++ db/migrate/20200417044119_create_monthly_votes_reports.rb
@@ -1,7 +1,7 @@
 class CreateMonthlyVotesReports < ActiveRecord::Migration[6.0]
   def change
     create_table :monthly_votes_reports do |t|
-      t.references :charity_id, foreign_key: true, index: true, null: false
+      t.references :charity, foreign_key: true, index: true, null: false
       t.integer :votes_count, default: 0
       t.date :report_month
       t.string :status, null: false
