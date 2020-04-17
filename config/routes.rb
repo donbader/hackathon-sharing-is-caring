73762333d1b87b0225cfcd100083c9db042f90b1
Juan <juanyong@xfers.com>
2020-04-17 12:43:41 +0800

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :charities, only: [:index, :show, :create, :update, :delete]
 end
