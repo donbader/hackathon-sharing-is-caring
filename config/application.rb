788790f988c9f5e7defa2eb22dc046905d45f546
Corey Feng <corey@xfers.com>
2020-04-17 12:18:08 +0800

    Suppress warning

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Warning[:deprecated] = false
 
 module HackathonSharingIsCaring
   class Application < Rails::Application
